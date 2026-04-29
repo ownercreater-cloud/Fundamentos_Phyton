3efa98349fddf4039b11686ab7be50f7fb7870c0
David Lopez Chaparro <ownercreatermail.com>
2026-04-29 09:09:42 -0500

    new var

--- fundamentos.py
+++ fundamentos.py
@@ -1,1 +1,18 @@
-print("hola mundo")
+nombre = "Juan David"
+apellido = "Lopez Chaparro"
+edad = "18"
+altura = "1.80"
+activo = True
+correo = "ownercreater@gmail.com"
+telefono = "3208829858"
+
+print(type(nombre), nombre)
+print(type(apellido), apellido)
+print(type(edad), edad)
+print(type(altura), altura)
+print(type(activo), activo)
+print(type(correo), correo)
+print(type(telefono), telefono)
+
+telefono = int(telefono)
+print(type(telefono))
